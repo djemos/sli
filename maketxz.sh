0211e09f14fe628da11de61c71e3a90f5f817898
djemos <dijemos@gmail.com>
2016-11-02 10:34:15 +0200

    update maketxz.sh

--- maketxz.sh
+++ maketxz.sh
@@ -17,15 +17,15 @@ RLZ=1dj
 	mkdir install
 
 cat <<EOF > install/slack-desc
-slackware-live: "Slackware Live (live DVD/USB/NFS build and install tool)"
-slackware-live: "It is written for Slackware / Slackware64 / Slackware-ARM Linux:"
-slackware-live: "support GPT and legacy MBR partitionned disks, UEFI and CSM "
-slackware-live: "(legacy / BIOS) booting, contains all the necessary to convert an"
-slackware-live: "installed system to a live system and vice-versa;"
-slackware-live: "it doesn’t need any kernel recompile, but it support seamlessly"
-slackware-live: "AUFS if your kernel support them; no changes are needed on the system"
-slackware-live: "to make live; the live system boots like an installed one"
-slackware-live: "(100% compatible with stock Slackware); support persistent changes"
+sli: "Slackware Live (live DVD/USB/NFS build and install tool)"
+sli: "It is written for Slackware / Slackware64 / Slackware-ARM Linux:"
+sli: "support GPT and legacy MBR partitionned disks, UEFI and CSM "
+sli: "(legacy / BIOS) booting, contains all the necessary to convert an"
+sli: "installed system to a live system and vice-versa;"
+sli: "it doesn’t need any kernel recompile, but it support seamlessly"
+sli: "using SquashFS and OverlayFS; no changes are needed on the system"
+sli: "to make live; the live system boots like an installed one"
+sli: "(100% compatible with stock Slackware); support persistent changes"
 EOF
 
 	/sbin/makepkg -l y -c n ../sli-$VER-$ARCH-$RLZ.txz
