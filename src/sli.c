b8125ae80a59ff1a64e1cab8a0d110db58473aa2
Dimitris Tzemos <djemos@users.noreply.github.com>
2022-09-10 11:56:00 +0300

    Update sli.c

--- src/sli.c
+++ src/sli.c
@@ -1,6 +1,6 @@
 /*
 SLI (Salix-Live installer) GUI version for salix.
-Copyright (C) 2020 Dimitris Tzemos <dijemos@gmail.com>
+Copyright (C) 2022 Dimitris Tzemos <dijemos@gmail.com>
 This program is free software: you can redistribute it and/or modify it under the terms 
 of the GNU General Public License as published by the Free Software Foundation, 
 either version 2 of the License, or (at your option) any later version.
